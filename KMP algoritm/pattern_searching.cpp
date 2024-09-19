423a0c4eb26a5e83722c0a837609c8c6fb6983f8
Suman Roy <138618510+sumanroy0624@users.noreply.github.com>
2024-09-19 16:13:56 +0530

    Update pattern_searching.cpp

--- KMP algoritm/pattern_searching.cpp
+++ KMP algoritm/pattern_searching.cpp
@@ -1,1 +1,43 @@
 
+class Solution {
+public:
+    vector<int> lps(string patt){
+        int n=patt.size();
+        vector<int>lps(n,0);
+        int pre=0;
+        int suf=1;
+        while(suf<n){
+            if(patt[pre]==patt[suf]){
+                lps[suf]=pre+1;
+                suf++,pre++;
+            }else{
+                if(pre==0){
+                    lps[suf]=0;
+                    suf++;
+                }else{
+                    pre=lps[pre-1];
+                }
+            }
+        }
+        return lps;
+    }
+    int strStr(string s, string patt) {
+        vector<int>psl=lps(patt);
+        int i=0;
+        int j=0;
+        while(i<s.size() && j<patt.size()){
+           if(s[i]==patt[j]){
+            i++;
+            j++;
+           }else{
+             if(j==0){
+                i++;
+             }else{
+                j=psl[j-1];
+             }
+           }
+           if(j==patt.size())return i-j;
+        }
+        return -1;
+    }
+};
