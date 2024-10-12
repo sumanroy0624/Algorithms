ae2e800c649c7365681a4d6f3a0e9334d90b5bff
Suman Roy <138618510+sumanroy0624@users.noreply.github.com>
2024-10-13 01:34:39 +0530

    Update Longest_dublicate_substring.cpp

--- Rabin karp/Longest_dublicate_substring.cpp
+++ Rabin karp/Longest_dublicate_substring.cpp
@@ -4,38 +4,36 @@ Problem Link: https://leetcode.com/problems/longest-duplicate-substring/
 Reference Video : https://youtu.be/dcOSQjhfBcQ?si=-mRjlxVe5wzG_5QU
 
 */
-
 #define ll long long
 ll mod=1e9+7;
 class Solution {
 public:
-    string f(string &s, ll len) {
-    ll p = 31; 
-    ll pow = 1;
-    ll hash = 0;
-
-    for (int i = 0; i < len; i++) {
-        hash = (hash * p + (s[i] - 'a' + 1)) % mod;
-        if (i < len - 1) pow = (pow * p) % mod; 
-    }
-    unordered_map<ll, vector<ll>> mp;
-    mp[hash].push_back(0);
-
-    ll sp = 0;
-    for (ll ep = len; ep < s.size(); ep++) {
-        hash = (hash - (s[sp] - 'a' + 1) * pow % mod + mod) % mod; 
-        hash = (hash * p + (s[ep] - 'a' + 1)) % mod; 
-        sp++;
-        if (mp.find(hash) != mp.end()) {
-            for (auto index : mp[hash]) {
-                if (s.substr(index, len) == s.substr(sp, len)) {
-                    return s.substr(sp, len);
-                }
-            }
-        }
-        mp[hash].push_back(sp);
-    }
-    return "-1";
+   string f(string &s, ll len) {
+	ll p = 31;
+	ll pow = 1;
+	ll hash = 0;
+	for (int i = len - 1; i >= 0; i--) {
+		hash = (hash + (s[i] - 'a' + 1) * pow) % mod;
+		if (i > 0)pow = (pow * p) % mod;
+	}
+	ll sp = 0;
+	ll ep = len;
+	unordered_map<int, vector<int>>mp;
+	mp[hash].push_back(0);
+	while (ep < s.size()) {
+		hash = ((hash - (s[sp] - 'a' + 1) * pow) % mod + mod) % mod;
+		hash = (hash * p) % mod;
+		hash = (hash + (s[ep] - 'a' + 1)) % mod;
+		sp++;
+		for (auto it : mp[hash]) {
+			if (s.substr(it, len) == s.substr(sp, len)) {
+				return s.substr(sp, len);
+			}
+		}
+		ep++;
+		mp[hash].push_back(sp);
+	}
+	return "-1";
 }
 
 
